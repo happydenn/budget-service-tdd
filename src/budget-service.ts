d68320ca13f71bffc5e352ca562ca594e687a900
Denny Tsai <denny.tsai@linecorp.com>
2018-08-28 15:57:11 +0800

    ✨ 2018-02-01 - 2018-02-28

--- src/budget-service.ts
+++ src/budget-service.ts
@@ -1,9 +1,29 @@
-import { Moment } from "moment";
+import moment = require('moment')
 
 export class BudgetService {
+  repo: IRepo
 
-  queryBudget(start: Moment, end: Moment): any {
+  constructor(repo) {
+    this.repo = repo
+  }
+
+  queryBudget(start: moment.Moment, end: moment.Moment): any {
+    const budgets = this.repo.getAll()
+
+    if (start.format('YYYYMM') === end.format('YYYYMM')) {
+      const match = budgets.filter(b => b.yearMonth === start.format('YYYYMM'))
+      if (match.length > 0) {
+        return match[0].amount
+      }
+    }
+
     return 310
   }
 
+}
+
+class IRepo {
+  getAll() {
+    return []
+  }
 }
--- src/budget.test.ts
+++ src/budget.test.ts
@@ -2,9 +2,26 @@ import test from 'ava'
 import { BudgetService } from './budget-service';
 import moment = require('moment')
 
+const fakeRepo = {
+  getAll() {
+    return [
+      { yearMonth: '201801', amount: 310 },
+      { yearMonth: '201802', amount: 28 },
+      { yearMonth: '201807', amount: 3100 },
+    ]
+  }
+}
+
 test('2018/01/01 - 2018/01/31', (t) => {
-  const service = new BudgetService()
+  const service = new BudgetService(fakeRepo)
   const actual = service.queryBudget(moment('2018-01-01'), moment('2018-01-31'))
 
   t.is(actual, 310)
+})
+
+test('2018/02/01 - 2018/02/28', (t) => {
+  const service = new BudgetService(fakeRepo)
+  const actual = service.queryBudget(moment('2018-02-01'), moment('2018-02-28'))
+
+  t.is(actual, 28)
 })
